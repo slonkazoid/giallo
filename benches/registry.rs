264ea6d0148affbd19d1c9f7061092be3e5bbe34
slonkazoid <slonkazoid@slonk.ing>
2026-02-20 19:37:22 +0300

    remove serialization benchmark

--- benches/registry.rs
+++ benches/registry.rs
@@ -1,20 +1,7 @@
 use criterion::{Criterion, criterion_group, criterion_main};
 use giallo::Registry;
 
-fn ser_benchmark(c: &mut Criterion) {
-    let registry = Registry::builtin().unwrap();
-
-    c.bench_function("registry serialization", |b| {
-        b.iter(|| {
-            let compressed = registry
-                .dump_to_bytes(None)
-                .expect("Failed to dump registry");
-            std::hint::black_box(compressed);
-        })
-    });
-}
-
-fn deser_benchmark(c: &mut Criterion) {
+fn registry_benchmark(c: &mut Criterion) {
     let dump = include_bytes!("../builtin.zst");
 
     c.bench_function("registry deserialization", |b| {
@@ -26,5 +13,5 @@ fn deser_benchmark(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, deser_benchmark, ser_benchmark);
+criterion_group!(benches, registry_benchmark, ser_benchmark);
 criterion_main!(benches);
